2b1053acd732a7bdfcee0a60cf41dcfe96a35629
Matthes Vogel <matthesv@gmail.com>
2025-01-26 17:49:20 +0100

    Update version to 1.0.16; adjust margin in CSS and change feedback button background color

--- feedback-voting.php
+++ feedback-voting.php
@@ -3,7 +3,7 @@
 Plugin Name: Feedback Voting
 Plugin URI:  https://www.abg.de
 Description: Bietet ein einfaches "Hat Ihnen diese Antwort geholfen?" (Ja/Nein) Feedback-Voting
-Version:     1.0.15
+Version:     1.0.16
 Author:      Matthes Vogel
 Text Domain: feedback-voting
 */
